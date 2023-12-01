d892b0db5825a4d272028966423a4d70f2a874dc
Tekali7 <alihan.teker@hotmail.com>
2023-12-01 17:51:58 +0100

    Change structure of the script to increase readability

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
-// Wait for the DOM to fully load before executing the code (Adapted from the walkthrough project)
-document.addEventListener("DOMContentLoaded", function () {
 
-// Set sec to 0
-let sec = 0;
+let sec = 0; // Set sec to 0
+let obstacle = document.getElementById("obstacle"); // Get the obstacle
+let player = document.getElementById("player"); // Get the player character 
+let obstaclePosition = 2200; // Starting position of the obstacle
+let playerPosition = 0; // Starting position of the player character
+
+document.addEventListener("DOMContentLoaded", function () { // Wait for the DOM to fully load before executing the code (Adapted from the walkthrough project)
 
     // if val is greater than 9 return val, else with string "0" infront of val
     function timer(val) {
@@ -13,23 +16,21 @@ let sec = 0;
     }
     }
 
-    // Divide sec by 60 
+    // Timer 
     function updateScore() {
     document.getElementById("score").innerHTML = "Current Score: " + timer(sec % 60);
     sec++;
     }
-
+    
     // Call the updateScore function every second
 setInterval(updateScore, 1000);
 
-    // Get the obstacle
-    let obstacle = document.getElementById("obstacle");
-
-    // Starting position of the obstacle
-    let obstaclePosition = 2200;
-
     // Learned from java2s setInterval() Move element
-    let obstacleMoveInterval = setInterval(function () {
+    let obstacleMoveInterval = setInterval(updateObstaclePosition, 10); 
+
+    /*WRITE DOCSTRING HEREEEEEEEEEEEEEEEEEEEEE*/
+
+    function updateObstaclePosition() {
         obstaclePosition -= 5;
 
         // Update the obstacles right position
@@ -39,24 +40,19 @@ setInterval(updateScore, 1000);
         if (obstaclePosition <= -150) {
             obstaclePosition = 2200;
         }
-    }, 10);
+    }
 
-    // Get the player character
-    let player = document.getElementById("player");
+    // Function, move character up in y if position is zero (Jump)
+    window.addEventListener("click", playerJump);
 
-    // Starting position of the player character
-    let playerPosition = 0;
-
-    // Function, move character up in y if position is zero
-    window.addEventListener("click", function() {
-        if(playerPosition === 0){
+    function playerJump(){
+        if (playerPosition === 0){
             playerPosition = 200;
-            player.style.bottom = playerPosition + "px";
+            player.style.bottom = playerPosition + "px";}
 
             setTimeout(function() {
                 playerPosition = 0;
                 player.style.bottom = playerPosition + "px";
-            }, 700);
+            }, 750);
         }
     });
-});
