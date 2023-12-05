428d5912966914ef3fbfd23be85b5d657efc294c
Tekali7 <alihan.teker@hotmail.com>
2023-12-05 03:20:34 +0100

    Add sweetalert to replace normal alerts

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,134 @@
-
-let obstacle = document.getElementById("obstacle"); // Get the obstacle
-let player = document.getElementById("player"); // Get the player character
+let obstacle = document.getElementById("obstacle");
+let player = document.getElementById("player");
 let closeModalButton = document.getElementById("closeModal");
 let modal = document.getElementById("modal-box");
 let overlay = document.getElementById("overlay");
 
-let sec; // Declare sec
-let obstaclePosition; // Declare obstacle position 
-let playerPosition; // Declare player position
+let sec;
+let obstaclePosition;
+let playerPosition;
 let obstacleMoveInterval;
 let playerTop;
 let obstacleLeft;
 let scoreUpdateInterval;
 let isModalOpen = false;
+let updateScoreFlag = true;
 
-document.addEventListener("DOMContentLoaded", loadGame); // Wait for the DOM to fully load before executing the code (Adapted from the walkthrough project)
+document.addEventListener("DOMContentLoaded", loadGame);
 closeModalButton.addEventListener("click", closeModal);
-window.addEventListener("click", playerJump); // Function, move character up in y if position is zero (Jump)
+window.addEventListener("click", playerJump);
 window.addEventListener("keydown", checkUpKey);
-setInterval(updateScore, 250); // Call the updateScore function every quarter of a second
-setInterval(checkCollision, 10); // Check if player and obstacle touch every 10ms
+setInterval(updateScore, 250);
+let collisionCheckInterval = setInterval(checkCollision, 10);
 
 function loadGame() {
     showModal();
 
     sec = 0;
-    obstaclePosition = 1350; // Starting position of the obstacle
-    playerPosition = 0; // Starting position of the player character
-    obstacleMoveInterval = setInterval(updateObstaclePosition, 10); // Adapted from java2s.com setInterval() move element chapter
+    obstaclePosition = 1350;
+    playerPosition = 0;
+    obstacleMoveInterval = setInterval(updateObstaclePosition, 10);
     scoreUpdateInterval = setInterval(updateScore, 250);
 }
 
-function showModal(){
+function showModal() {
     modal.style.display = "block";
-    overlay.style.display ="block";
+    overlay.style.display = "block";
     isModalOpen = true;
 }
 
-function closeModal(){
+function closeModal() {
     modal.style.display = "none";
     overlay.style.display = "none";
     isModalOpen = false;
 }
 
-    /*
-    * Resets obstacle position to the right 
-    * if a certain point is reached
-    */
 function updateObstaclePosition() {
-    if (!isModalOpen){
-    obstaclePosition -= 5;
-    obstacle.style.left = obstaclePosition + "px"; // Update the obstacles right position
+    if (!isModalOpen) {
+        obstaclePosition -= 5;
+        obstacle.style.left = obstaclePosition + "px";
 
-    // Resetting the obstacle back to the right
-    if (obstaclePosition <= -150) {
-        obstaclePosition = 1350;
-}
+        if (obstaclePosition <= -150) {
+            obstaclePosition = 1350;
+        }
 
-    // Make the obstacle gradually disappear at x+5
-    if (obstaclePosition <= 5){
-        obstacle.style.opacity = "0";
-    } else {
-        obstacle.style.opacity = "1";
+        if (obstaclePosition <= 5) {
+            obstacle.style.opacity = "0";
+        } else {
+            obstacle.style.opacity = "1";
         }
     }
 }
 
-function playerJump(){
-    if (!isModalOpen){
-
-        if (playerPosition === 0){
+function playerJump() {
+    if (!isModalOpen) {
+        if (playerPosition === 0) {
             playerPosition = 200;
-            player.style.bottom = playerPosition + "px";}
+            player.style.bottom = playerPosition + "px";
+        }
 
-            setTimeout(function() {
-                playerPosition = 0;
-                player.style.bottom = playerPosition + "px";
-            }, 750);
+        setTimeout(function () {
+            playerPosition = 0;
+            player.style.bottom = playerPosition + "px";
+        }, 750);
     }
 }
 
-function checkUpKey(event){
-    if (event.key === "ArrowUp"){
+function checkUpKey(event) {
+    if (event.key === "ArrowUp") {
         playerJump();
     }
 }
 
-// if val is greater than 9 return val, else with string "0" infront of val
 function timer(val) {
     if (val > 9) {
         return val;
-    } else{
+    } else {
         return "0" + val;
     }
 }
 
-// Score
 function updateScore() {
-    if (!isModalOpen){
-    document.getElementById("score").innerHTML = "Current Score: " + timer(sec);
-    ++sec;
+    if (!isModalOpen && updateScoreFlag) {
+        document.getElementById("score").innerHTML = "Current Score: " + timer(sec);
+        ++sec;
     }
 }
 
-// Collision detection
-function checkCollision(){
-    if (!isModalOpen){
+let collisionDetected = false;
 
-    playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
-    obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
-    
+function checkCollision() {
+    if (!isModalOpen) {
+        playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+        obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
 
-    if(obstacleLeft < 209 && obstacleLeft > 0 && playerTop >= 210){
+        if (obstacleLeft < 209 && obstacleLeft > 0 && playerTop >= 210) {
+            if (!collisionDetected) {
+                collisionDetected = true;
 
-        clearInterval(obstacleMoveInterval); // Stop the obstacle from moving
-        clearInterval(scoreUpdateInterval);
-        alert("Game over, your final score is: " + timer(--sec));
+                clearInterval(obstacleMoveInterval);
+                clearInterval(scoreUpdateInterval);
+                clearInterval(collisionCheckInterval)
+
+                // Set updateScoreFlag to false to stop score updates
+                updateScoreFlag = false;
+
+                setTimeout(() => {
+                    Swal.fire({
+                        title: 'Game Over',
+                        text: 'Your final score is: ' + timer(--sec),
+                        icon: 'error',
+                        confirmButtonText: 'Try Again',
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            location.reload(); // Reload the page when "Try Again" is clicked
+                        }
+                    });
+                }, 200);
+                
+            }
+        } else {
+            collisionDetected = false;
         }
     }
 }
